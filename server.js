00f8b1548ca14ae6449876994373798606992d99
MokubaTek <103837321+MokubaTek@users.noreply.github.com>
2022-07-04 14:45:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) =>{
     console.log('new Connection')
-    res.end('Hello!! does this still work???')
+    res.end('ready to start messing it up')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening...'))
